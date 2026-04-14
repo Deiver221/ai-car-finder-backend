577d2982e4fbf00b27a9b584ccb2f2f4460ddc61
Deiverrr <delvin2812@gmail.com>
2026-04-14 20:25:45 -0300

    Fix API Status

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return response()->json([
-        'message' => 'AI Car Finder API',
-        'status' => 'running'
-    ]);
+    return('');
 });
