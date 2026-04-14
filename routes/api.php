887a43a545c95f85909e937d605d65ab56a66c9f
Deiverrr <delvin2812@gmail.com>
2026-04-14 20:10:54 -0300

    Add API Status 2

--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,11 @@
 <?php
 
 use App\Http\Controllers\Api\CarController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return response()->json([
-        'message' => 'AI Car Finder API',
-        'status' => 'running'
-    ]);
+Route::get('/user', function (Request $request) {
+    return $request->user();
 })->middleware('auth:sanctum');
 
 Route::get('/cars', [CarController::class, 'index']);
